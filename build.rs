8201e319361d0d53424370a3bc51d544db736858
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-03-25 07:02:18 +0100

    Make resource compiler work without running vcvars64.bat

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
-use cc;
-use embed_resource;
-
 fn main() {
     generate_bindings();
-    cc::windows_registry::find_tool()
+    let target = std::env::var("TARGET").unwrap();
+    if let Some(tool) = cc::windows_registry::find_tool(target.as_str(), "cl.exe") {
+        for (key, value) in tool.env() {
+            std::env::set_var(key, value);
+        }
+    }
     embed_resource::compile("src/realearn.rc");
 }
 
