bcefd908cfe9bc9b56c2ff63620ab72e6142da7d
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-06-15 22:06:55 +0200

    Fix some tempo-related normalizations

--- src/domain/reaper_target.rs
+++ src/domain/reaper_target.rs
@@ -660,7 +660,10 @@ impl Target for ReaperTarget {
         use ReaperTarget::*;
         match self {
             FxParameter { param } => param.step_size().map(UnitValue::new),
-            Tempo { .. } => Some(UnitValue::MIN),
+            Tempo { .. } => {
+                let span = Bpm::MAX.get() - Bpm::MIN.get();
+                Some(UnitValue::new(1.0 / span))
+            }
             FxPreset { fx } => {
                 // `+ 1` because "no preset" is also a possible value
                 Some(UnitValue::new(1.0 / (fx.preset_count() + 1) as f64))
