980c22827a484c31535df5e77c68107bfae612c7
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-06-23 13:20:40 +0200

    Improve arrows, disable them on Linux again

--- src/infrastructure/ui/constants.rs
+++ src/infrastructure/ui/constants.rs
@@ -5,23 +5,31 @@ pub const MAIN_PANEL_DIMENSIONS: Dimensions<DialogUnits> =
     Dimensions::new(DialogUnits(449), DialogUnits(413));
 
 pub mod symbols {
-    #[cfg(target_family = "windows")]
-    pub const ARROW_UP_SYMBOL: char = '🡹';
-    #[cfg(target_family = "unix")]
-    pub const ARROW_UP_SYMBOL: char = '⬆';
+    #[cfg(target_os = "windows")]
+    pub const ARROW_UP_SYMBOL: &str = "🡹";
+    #[cfg(target_os = "macos")]
+    pub const ARROW_UP_SYMBOL: &str = "⬆";
+    #[cfg(target_os = "linux")]
+    pub const ARROW_UP_SYMBOL: &str = "Up";
 
-    #[cfg(target_family = "windows")]
-    pub const ARROW_DOWN_SYMBOL: char = '🡻';
-    #[cfg(target_family = "unix")]
-    pub const ARROW_DOWN_SYMBOL: char = '⬇';
+    #[cfg(target_os = "windows")]
+    pub const ARROW_DOWN_SYMBOL: &str = "🡻";
+    #[cfg(target_os = "macos")]
+    pub const ARROW_DOWN_SYMBOL: &str = "⬇";
+    #[cfg(target_os = "linux")]
+    pub const ARROW_DOWN_SYMBOL: &str = "Down";
 
-    #[cfg(target_family = "windows")]
-    pub const ARROW_LEFT_SYMBOL: char = '🡸';
-    #[cfg(target_family = "unix")]
-    pub const ARROW_LEFT_SYMBOL: char = '⬅';
+    #[cfg(target_os = "windows")]
+    pub const ARROW_LEFT_SYMBOL: &str = "🡸";
+    #[cfg(target_os = "macos")]
+    pub const ARROW_LEFT_SYMBOL: &str = "⬅";
+    #[cfg(target_os = "linux")]
+    pub const ARROW_LEFT_SYMBOL: &str = "<=";
 
-    #[cfg(target_family = "windows")]
-    pub const ARROW_RIGHT_SYMBOL: char = '🡺';
-    #[cfg(target_family = "unix")]
-    pub const ARROW_RIGHT_SYMBOL: char = '⮕';
+    #[cfg(target_os = "windows")]
+    pub const ARROW_RIGHT_SYMBOL: &str = "🡺";
+    #[cfg(target_os = "macos")]
+    pub const ARROW_RIGHT_SYMBOL: &str = "⮕";
+    #[cfg(target_os = "linux")]
+    pub const ARROW_RIGHT_SYMBOL: &str = "=>";
 }
