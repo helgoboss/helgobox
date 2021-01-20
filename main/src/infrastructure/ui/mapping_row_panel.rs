13bd299958bdc4babe3bb0d983e014ae548fb10b
Benjamin Klum <benjamin.klum@helgoboss.org>
2021-01-20 14:05:41 +0100

    #101 Fix mapping row painting on Linux

--- main/src/infrastructure/ui/mapping_row_panel.rs
+++ main/src/infrastructure/ui/mapping_row_panel.rs
@@ -497,6 +497,16 @@ impl View for MappingRowPanel {
         )
     }
 
+    // On Linux, WM_ERASEBKGND is not called, so we need to do it in WM_PAINT.
+    #[cfg(target_os = "linux")]
+    fn paint(self: SharedView<Self>) -> bool {
+        util::view::erase_background_with(
+            self.view.require_window().raw(),
+            std::ptr::null_mut(),
+            util::view::rows_brush(),
+        )
+    }
+
     fn control_color_static(
         self: SharedView<Self>,
         hdc: raw::HDC,
