79072e674bd00ebfb642871a2bd87c43d3ebd8f3
Benjamin Klum <benjamin.klum@helgoboss.org>
2021-10-27 02:22:08 +0200

    Remove seashash

--- main/src/base/hash_util.rs
+++ main/src/base/hash_util.rs
@@ -1,8 +1,7 @@
 use std::hash::{Hash, Hasher};
 
-// TODO-high We should replace this with two-x-hash!
 pub fn calculate_non_crypto_hash<T: Hash>(t: &T) -> u64 {
-    let mut s = seahash::SeaHasher::new();
-    t.hash(&mut s);
-    s.finish()
+    let mut hasher = twox_hash::XxHash64::default();
+    t.hash(&mut hasher);
+    hasher.finish()
 }
