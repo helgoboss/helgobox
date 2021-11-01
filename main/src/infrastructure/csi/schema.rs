d879d0903a641a24c148fc5761134f3ac4c653b6
Benjamin Klum <benjamin.klum@helgoboss.org>
2021-11-01 22:06:22 +0100

    CSI MST parsing draft

---
+++ main/src/infrastructure/csi/schema.rs
@@ -0,0 +1,58 @@
+use helgoboss_midi::RawShortMessage;
+use std::ops::RangeInclusive;
+
+#[derive(PartialEq, Debug)]
+pub struct Widget {
+    pub name: String,
+    pub capabilities: Vec<Capability>,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Capability {
+    Press {
+        press: RawShortMessage,
+        release: Option<RawShortMessage>,
+    },
+    FbTwoState {
+        on: RawShortMessage,
+        off: RawShortMessage,
+    },
+    Encoder {
+        main: RawShortMessage,
+        decrements: Acceleration,
+        increments: Acceleration,
+    },
+    FbEncoder {
+        max: RawShortMessage,
+    },
+    Toggle {
+        on: RawShortMessage,
+    },
+    Fader14Bit {
+        max: RawShortMessage,
+    },
+    FbFader14Bit {
+        max: RawShortMessage,
+    },
+    Touch {
+        on: RawShortMessage,
+        off: RawShortMessage,
+    },
+    FbMcuDisplayLower {
+        index: u32,
+    },
+    FbMcuDisplayUpper {
+        index: u32,
+    },
+    FbMcuTimeDisplay,
+    FbMcuVuMeter {
+        index: u32,
+    },
+    Unknown(String),
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Acceleration {
+    Sequence(Vec<u8>),
+    Range(RangeInclusive<u8>),
+}
