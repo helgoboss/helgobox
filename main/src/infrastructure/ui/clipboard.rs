bb6ff459a912aaa2cfc05c3796c82e71022a39e1
Benjamin Klum <benjamin.klum@helgoboss.org>
2021-12-06 19:27:29 +0100

    Make clipboard work on Linux again by reusing it

--- main/src/infrastructure/ui/clipboard.rs
+++ main/src/infrastructure/ui/clipboard.rs
@@ -1,13 +1,17 @@
+use std::sync::Mutex;
 use arboard::Clipboard;
+use once_cell::sync::Lazy;
+
+static CLIPBOARD: Lazy<Mutex<Clipboard>> = Lazy::new(|| Mutex::new(Clipboard::new().expect("couldn't create clipboard")));
 
 pub fn copy_text_to_clipboard(text: String) {
-    let mut clipboard = Clipboard::new().expect("couldn't create clipboard");
+    let mut clipboard = CLIPBOARD.lock().unwrap();
     clipboard
         .set_text(text)
         .expect("couldn't set clipboard contents");
 }
 
 pub fn get_text_from_clipboard() -> Option<String> {
-    let mut clipboard = Clipboard::new().ok()?;
+    let mut clipboard = CLIPBOARD.lock().unwrap();
     clipboard.get_text().ok()
 }
