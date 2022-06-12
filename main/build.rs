2ffd504c9ebd8ebd4c2cb32f559af642f37646c8
Benjamin Klum <benjamin.klum@helgoboss.org>
2022-06-12 12:17:35 +0200

    #583 Complain on macOS and Linux if PHP not found at build time

--- main/build.rs
+++ main/build.rs
@@ -13,17 +13,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     fs::create_dir_all(&generated_dir)?;
     realearn_dialogs::generate_dialog_files(&generated_dir, bindings_file);
 
-    // On macOS and Linux, try to generate SWELL dialogs (needs PHP)
+    // On macOS and Linux, generate SWELL dialogs (needs PHP)
     #[cfg(target_family = "unix")]
-    if let Err(e) = generate_dialogs(&dialog_rc_file) {
-        println!("cargo:warning={}", e);
-    }
+    generate_dialogs(&dialog_rc_file);
 
     // Optionally generate bindings (e.g. from Cockos EEL)
     #[cfg(feature = "generate")]
     codegen::generate_bindings();
 
-    // Embed or compile dialogs
+    // Embed (Windows) or compile (Linux/macOS) dialogs
     #[cfg(target_family = "windows")]
     embed_dialog_resources(&dialog_rc_file);
     #[cfg(target_family = "unix")]
@@ -158,17 +156,16 @@ mod codegen {
 /// Generates dialog window C++ code from resource file using SWELL's PHP-based dialog generator
 /// (too obscure to be ported to Rust).
 #[cfg(target_family = "unix")]
-pub fn generate_dialogs(rc_file: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+pub fn generate_dialogs(rc_file: impl AsRef<Path>) {
     // Use PHP to translate SWELL-compatible RC file to C++
     let result = std::process::Command::new("php")
         .arg("lib/WDL/WDL/swell/swell_resgen.php")
         .arg(rc_file.as_ref())
         .output()
-        .expect("PHP dialog translator result not available");
+        .expect("PHP dialog translator result not available, probably PHP not installed");
     if !result.status.success() {
-        return Err("PHP dialog generation failed (PHP not installed or script failed)".into());
+        panic!("PHP dialog generation failed (PHP available but generator failed)".into());
     }
-    Ok(())
 }
 
 mod util {
