797b08b6d847169a967845b5c57c77f878e16597
Benjamin Klum <benjamin.klum@helgoboss.org>
2023-05-09 15:26:50 +0200

    #842 Use 128-bit xxhash instead of 160-bit SHA1 for directory structure

--- main/src/base/file_util.rs
+++ main/src/base/file_util.rs
@@ -1,4 +1,4 @@
-use sha1::{Digest, Sha1};
+use crate::base::hash_util;
 use walkdir::DirEntry;
 
 pub fn is_hidden(entry: &DirEntry) -> bool {
@@ -10,18 +10,15 @@ pub fn is_hidden(entry: &DirEntry) -> bool {
 }
 
 /// Converts something like "test" to something like
-/// "a9/4a/8fe5ccb19ba61c4c0873d391e987982fbbd3.RfxChain" for the purpose to not get too many
+/// "a9/4a/8fe5ccb19ba61c4c0873d391e987.RfxChain" for the purpose to not get too many
 /// files in one directory.
 pub fn hash_to_dir_structure(input: impl AsRef<[u8]>, suffix: &str) -> String {
-    let mut hasher = Sha1::new();
-    hasher.update(input);
-    let array = hasher.finalize();
-    format!(
-        "{:x}/{:x}/{}{suffix}",
-        array[0],
-        array[1],
-        hex::encode(&array[2..])
-    )
+    let hash = hash_util::calculate_persistent_non_crypto_hash_one_shot(input.as_ref());
+    let first_byte = hash.rotate_left(8) & 0xff;
+    let second_byte = hash.rotate_left(16) & 0xff;
+    // Remaining: 112 bits = 14 bytes = 28 hex chars
+    let remaining = hash & 0xffffffffffffffffffffffffffff;
+    format!("{first_byte:02x}/{second_byte:02x}/{remaining:028x}{suffix}")
 }
 
 #[cfg(test)]
@@ -32,7 +29,7 @@ mod tests {
     fn hash_to_dir_structure_simple() {
         assert_eq!(
             hash_to_dir_structure("test", ".RfxChain"),
-            "a9/4a/8fe5ccb19ba61c4c0873d391e987982fbbd3.RfxChain".to_string()
+            "6c/78/e0e3bd51d358d01e758642b85fb8.RfxChain".to_string()
         );
     }
 }
--- main/src/base/hash_util.rs
+++ main/src/base/hash_util.rs
@@ -7,6 +7,14 @@ pub fn calculate_non_crypto_hash_one_shot(payload: &[u8]) -> u64 {
     xxhash_rust::xxh3::xxh3_64(payload)
 }
 
+/// Calculates a 128-bit non-crypto hash directly from the given bytes suitable for persistence.
+///
+/// This implementation must not change!
+pub fn calculate_persistent_non_crypto_hash_one_shot(payload: &[u8]) -> u128 {
+    // Don't change! It's used e.g. for file names.
+    xxhash_rust::xxh3::xxh3_128(payload)
+}
+
 /// Calculates a 64-bit non-crypto hash from the given hashable type.
 ///
 /// If you already have a slice of bytes, use the one-shot version instead.
