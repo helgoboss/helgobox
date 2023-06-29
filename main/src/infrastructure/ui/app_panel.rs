f1e9a63a90164d35a65e2bae5837a483698c28a0
Benjamin Klum <benjamin.klum@helgoboss.org>
2023-06-29 11:25:54 +0200

    Playtime: Further improve app embedding code

--- main/src/infrastructure/ui/app_panel.rs
+++ main/src/infrastructure/ui/app_panel.rs
@@ -21,14 +21,14 @@ use swell_ui::{SharedView, View, ViewContext, Window};
 #[derive(Debug)]
 pub struct AppPanel {
     view: ViewContext,
-    app: PlaytimeApp,
+    app: LoadedApp,
 }
 
 impl AppPanel {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let panel = Self {
             view: Default::default(),
-            app: PlaytimeApp::load()?,
+            app: LoadedApp::load()?,
         };
         Ok(panel)
     }
@@ -44,7 +44,7 @@ impl View for AppPanel {
     }
 
     fn opened(self: SharedView<Self>, window: Window) -> bool {
-        self.app.run(window).unwrap();
+        self.app.run_in_parent(window).unwrap();
         true
     }
 
@@ -63,63 +63,102 @@ impl View for AppPanel {
 }
 
 #[derive(Debug)]
-pub struct PlaytimeApp {
-    library: Library,
+pub struct LoadedApp {
+    main_library: Library,
+    dependencies: Vec<Library>,
 }
 
 // TODO-high-playtime Adjust
 
 #[cfg(target_os = "macos")]
-const APP_BASE_DIR: &str = "/Users/helgoboss/Documents/projects/dev/playtime/build/macos/Build/Products/Release/playtime.app";
+const APP_BASE_DIR: &str = "/Users/helgoboss/Documents/projects/dev/playtime/build/macos/Build/Products/Debug/playtime.app";
 
 #[cfg(target_os = "windows")]
 const APP_BASE_DIR: &str =
-    "C:\\Users\\benja\\Documents\\projects\\dev\\playtime\\build\\windows\\runner\\Release";
+    "C:\\Users\\benja\\Documents\\projects\\dev\\playtime\\build\\windows\\runner\\Debug";
 
-impl PlaytimeApp {
+impl LoadedApp {
     pub fn load() -> Result<Self, libloading::Error> {
         let app_base_dir = Path::new(APP_BASE_DIR);
-        let library = unsafe {
+        let (main_library, dependencies) = {
             #[cfg(target_os = "windows")]
             {
-                let lib1 = app_base_dir.join("flutter_windows.dll");
-                let lib2 = app_base_dir.join("url_launcher_windows_plugin.dll");
-                let lib3 = app_base_dir.join("playtime.dll");
-                let libs = vec![Library::new(lib1).unwrap(), Library::new(lib2).unwrap()];
-                let main_lib = Library::new(lib3);
-                dbg!(&libs);
-                main_lib
+                (
+                    "playtime.dll",
+                    ["flutter_windows.dll", "url_launcher_windows_plugin.dll"],
+                )
             }
             #[cfg(target_os = "macos")]
             {
-                let lib1 =
-                    app_base_dir.join("Contents/Frameworks/FlutterMacOS.framework/FlutterMacOS");
-                let lib2 = app_base_dir
-                    .join("Contents/Frameworks/url_launcher_macos.framework/url_launcher_macos");
-                let lib3 = app_base_dir.join("Contents/MacOS/playtime");
-                let libs = vec![Library::new(lib1).unwrap(), Library::new(lib2).unwrap()];
-                let main_lib = Library::new(lib3);
-                dbg!(&libs);
-                main_lib
+                (
+                    "Contents/MacOS/playtime",
+                    [
+                        "Contents/Frameworks/FlutterMacOS.framework/FlutterMacOS",
+                        "Contents/Frameworks/url_launcher_macos.framework/url_launcher_macos",
+                    ],
+                )
             }
-        }?;
-        dbg!(&library);
-        let playtime = Self { library };
-        Ok(playtime)
+        };
+        let app = unsafe {
+            LoadedApp {
+                dependencies: dependencies
+                    .into_iter()
+                    .filter_map(|dep| Library::new(app_base_dir.join(dep)).ok())
+                    .collect(),
+                main_library: Library::new(app_base_dir.join(main_library))?,
+            }
+        };
+        Ok(app)
     }
 
-    pub fn run(&self, parent_window: Window) -> Result<(), &'static str> {
-        // TODO-high-playtime Safely revert current working directory after that!
-        env::set_current_dir(APP_BASE_DIR).unwrap();
-        unsafe {
-            let symbol: Symbol<Run> = self
-                .library
-                .get(b"runPlaytime\0")
-                .map_err(|_| "failed to load run function")?;
-            symbol(parent_window.raw());
-        };
-        Ok(())
+    pub fn run_in_parent(&self, parent_window: Window) -> Result<(), &'static str> {
+        with_temporarily_changed_working_directory(APP_BASE_DIR, || {
+            prepare_app_launch();
+            let successful = unsafe {
+                let symbol: Symbol<RunInParent> = self
+                    .main_library
+                    .get(b"runAppInParent\0")
+                    .map_err(|_| "failed to load runAppInParent function")?;
+                symbol(parent_window.raw())
+            };
+            if !successful {
+                return Err("couldn't launch app");
+            }
+            Ok(())
+        })
     }
 }
 
-type Run = unsafe extern "stdcall" fn(hwnd: HWND) -> std::ffi::c_int;
+type RunInParent = unsafe extern "stdcall" fn(hwnd: HWND) -> bool;
+
+fn prepare_app_launch() {
+    #[cfg(target_os = "macos")]
+    {
+        // This is only necessary and only considered by Flutter Engine when Flutter is compiled in
+        // debug mode. In release mode, Flutter will work with AOT data embedded in the binary.
+        let env_vars = [
+            ("FLUTTER_ENGINE_SWITCHES", "3"),
+            ("FLUTTER_ENGINE_SWITCH_1", "snapshot-asset-path=Contents/Frameworks/App.framework/Versions/A/Resources/flutter_assets"),
+            ("FLUTTER_ENGINE_SWITCH_2", "vm-snapshot-data=vm_snapshot_data"),
+            ("FLUTTER_ENGINE_SWITCH_3", "isolate-snapshot-data=isolate_snapshot_data"),
+        ];
+        for (key, value) in env_vars {
+            env::set_var(key, value);
+        }
+    }
+}
+
+fn with_temporarily_changed_working_directory<R>(
+    new_dir: impl AsRef<Path>,
+    f: impl FnOnce() -> R,
+) -> R {
+    let previous_dir = env::current_dir();
+    let dir_change_was_successful = env::set_current_dir(new_dir).is_ok();
+    let r = f();
+    if dir_change_was_successful {
+        if let Ok(d) = previous_dir {
+            let _ = env::set_current_dir(d);
+        }
+    }
+    r
+}
