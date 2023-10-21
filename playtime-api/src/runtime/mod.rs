726d0ad874000ad2a0716c69e21a7df766686759
Benjamin Klum <benjamin.klum@helgoboss.org>
2023-10-21 17:59:26 +0200

    Playtime: Categorize TODOs

--- playtime-api/src/runtime/mod.rs
+++ playtime-api/src/runtime/mod.rs
@@ -9,7 +9,7 @@ pub struct PlaytimeRuntimeRoot(ClipPlayState);
 ///
 /// Order of enum variants is important to aggregate the play state of multiple clips in one slot.
 /// The slot always gets the "highest" play state.
-// TODO-high-clip-matrix We don't need this in the API because we use gRPC for runtime stuff. Factor it back
+// TODO-high-ms4 We don't need this in the API because we use gRPC for runtime stuff. Factor it back
 //  into the clip engine module.
 #[derive(
     Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Serialize, Deserialize, JsonSchema,
