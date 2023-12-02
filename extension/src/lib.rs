977a09ec71760113f2d1f66a5882121d93de75e9
Benjamin Klum <benjamin.klum@helgoboss.org>
2023-12-02 01:09:09 +0100

    Add Playtime toolbar button

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Context, Result};
+use ini::{Ini, ParseOption};
 use libloading::{Library, Symbol};
 use realearn_api::runtime::{HelgoboxApiPointers, HelgoboxApiSession};
 use reaper_fluent::{FreeFn, Reaper};
@@ -6,12 +7,13 @@ use reaper_low::{PluginContext, TypeSpecificPluginContext};
 use reaper_macros::reaper_extension_plugin;
 use reaper_medium::{
     reaper_str, AcceleratorBehavior, AcceleratorKeyCode, AddFxBehavior, CommandId, HookCommand,
-    OwnedGaccelRegister, ProjectContext, ReaperSession, TrackDefaultsBehavior, TrackFxChainType,
+    OwnedGaccelRegister, ProjectContext, ReaperSession, ReaperStr, TrackDefaultsBehavior,
+    TrackFxChainType,
 };
 use std::error::Error;
-use std::fs;
 use std::ptr::null_mut;
 use std::sync::OnceLock;
+use std::{cmp, fs};
 
 // Executing Drop not important because extensions always live until REAPER ends.
 static EXTENSION: OnceLock<HelgoboxExtension> = OnceLock::new();
@@ -39,19 +41,22 @@ impl HelgoboxExtension {
         medium_session.plugin_register_add_hook_command::<Self>()?;
         // Register actions
         let show_or_hide_playtime_command_id =
-            medium_session.plugin_register_add_command_id(reaper_str!("HB_SHOW_HIDE_PLAYTIME"))?;
+            medium_session.plugin_register_add_command_id(COMMAND_ID_SHOW_HIDE_PLAYTIME)?;
         medium_session.plugin_register_add_gaccel(OwnedGaccelRegister::with_key_binding(
             show_or_hide_playtime_command_id,
-            "Show/hide Playtime",
+            ACTION_LABEL_SHOW_HIDE_PLAYTIME,
             AcceleratorBehavior::Shift
                 | AcceleratorBehavior::Control
                 | AcceleratorBehavior::VirtKey,
             AcceleratorKeyCode::new(b'P' as _),
         ))?;
+        // Install reaper-fluent for global use
         let _ = Reaper::install_globally(medium_session);
+        // Add toolbar button
+        let _ = add_playtime_toolbar_button();
         // Return extension
         let extension = Self {
-            plugin_library: Some(eagerly_load_plugin_lib(&context).unwrap()),
+            plugin_library: eagerly_load_plugin_lib(&context).ok(),
             show_or_hide_playtime_command_id,
         };
         Ok(extension)
@@ -181,3 +186,68 @@ fn eagerly_load_plugin_lib(context: &PluginContext) -> Result<Library> {
     }
     Ok(plugin_library)
 }
+
+fn add_playtime_toolbar_button() -> Result<()> {
+    // Load toolbar button INI file
+    let reaper_menu_ini = Reaper::get()
+        .medium_reaper()
+        .get_resource_path(|p| p.join("reaper-menu.ini"));
+    let mut ini = Ini::load_from_file_opt(
+        &reaper_menu_ini,
+        ParseOption {
+            enabled_quote: false,
+            enabled_escape: false,
+        },
+    )?;
+    // Look through existing toolbar buttons
+    let toolbar_section = ini
+        .section_mut(Some("Main toolbar"))
+        .context("couldn't find main toolbar section")?;
+    let mut max_item_index = -1i32;
+    for (key, value) in toolbar_section.iter() {
+        let Some(toolbar_item) = ToolbarItem::parse_from_ini_prop(key, value) else {
+            continue;
+        };
+        if &toolbar_item.command[1..] == COMMAND_ID_SHOW_HIDE_PLAYTIME {
+            // Toolbar button exists already
+            return Ok(());
+        }
+        max_item_index = max_item_index.max(toolbar_item.index as _);
+    }
+    // Add new toolbar button
+    let next_item_index = max_item_index + 1;
+    toolbar_section.insert(
+        format!("item_{next_item_index}"),
+        format!("_{COMMAND_ID_SHOW_HIDE_PLAYTIME} {ACTION_LABEL_SHOW_HIDE_PLAYTIME}"),
+    );
+    ini.write_to_file(&reaper_menu_ini)?;
+    Reaper::get().medium_reaper().low().UpdateArrange();
+    Reaper::get().medium_reaper().low().UpdateTimeline();
+    Ok(())
+}
+
+struct ToolbarItem<'a> {
+    index: u32,
+    command: &'a str,
+    desc: &'a str,
+}
+
+impl<'a> ToolbarItem<'a> {
+    fn parse_from_ini_prop(key: &'a str, value: &'a str) -> Option<Self> {
+        let Some(("item", i)) = key.split_once('_') else {
+            return None;
+        };
+        let Some((command, desc)) = value.split_once(' ') else {
+            return None;
+        };
+        let item = ToolbarItem {
+            index: i.parse().ok()?,
+            command,
+            desc,
+        };
+        Some(item)
+    }
+}
+
+const COMMAND_ID_SHOW_HIDE_PLAYTIME: &str = "HB_SHOW_HIDE_PLAYTIME";
+const ACTION_LABEL_SHOW_HIDE_PLAYTIME: &str = "Show/hide Playtime";
