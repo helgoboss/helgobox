658f54e9af74fec752956df5770c08c0e2fda43d
Benjamin Klum <benjamin.klum@helgoboss.org>
2023-12-11 12:07:56 +0100

    Playtime: Deserialize matrix sequences

--- playtime-api/src/persistence/mod.rs
+++ playtime-api/src/persistence/mod.rs
@@ -21,17 +21,19 @@
 //!   work well with ReaLearn Script (`lua_serializer.rs`) **and** our Rust-to-Dart converter.
 //!   For the latter, we need to avoid things like `#[serde(flatten)]` or type parameters!
 
+mod serialization;
+
 use base64::engine::general_purpose::URL_SAFE_NO_PAD as BASE64_ENGINE;
 use base64::Engine;
 use chrono::{Local, NaiveDateTime, Utc};
+use serde::de::{SeqAccess, Visitor};
 use serde::ser::{SerializeSeq, SerializeTuple};
-use serde::{Deserialize, Serialize, Serializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_tuple::{Deserialize_tuple, Serialize_tuple};
 use std::cmp;
-use std::error::Error;
+use std::fmt::Formatter;
 use std::ops::Add;
 use std::path::PathBuf;
-use thiserror::Error;
 
 // TODO-medium Add start time detection
 // TODO-medium Add legato
@@ -99,106 +101,10 @@ pub struct MatrixSequenceData {
     pub events: Vec<MatrixSequenceEvent>,
 }
 
-#[derive(Clone, PartialEq, Debug, Deserialize)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct MatrixSequenceEvent {
     pub pulse_diff: u32,
     pub message: MatrixSequenceMessage,
-}
-
-/// A very compact representation of an event.
-///
-/// The compactness is achieved using tuple serialization.
-///
-/// Pro:
-///
-/// - If used with a CSV serializer, the outcome can be made look as "noise-less" as
-///   REAPER's MIDI sequence format (newline to separate events, space delimiter, unquoted strings).
-/// - If used with JSON/Lua serializer, the outcome is valid JSON/Lua while still being compact.
-///   No need for string embedding (which looks especially bad in JSON due to newline escaping).
-/// - If used with a binary serializer (e.g. bincode or msgpack), one can achieve a *really* compact
-///   serialization that  also tops REAPER's MIDI sequence format. That will come in handy with
-///   large undo histories or storage within RPP (in RPPs, we are base64-encoded, so the
-///   human-readable-text advantage is not present anyway).
-/// - TODO-high-ms3 Especially the last point could be desirable for MIDI sequences as well.
-///    Use serde for them, too!
-///
-/// Contra:
-///
-/// - Not self-describing. However: If embedded in some `Serialize` wrapper that evaluates
-///   `is_human_readable`, one could switch between this non-descriptive serialization style and a
-///   (derived) descriptive serialization style. So we can have both if we want.
-impl Serialize for MatrixSequenceEvent {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let hr = serializer.is_human_readable();
-        use MatrixSequenceMessage::*;
-        match self.message {
-            PanicMatrix => {
-                let mut seq = serializer.serialize_tuple(2)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                if hr {
-                    seq.serialize_element(&"pm")?;
-                } else {
-                    seq.serialize_element(&0u8)?;
-                }
-                seq.end()
-            }
-            StopMatrix => {
-                let mut seq = serializer.serialize_tuple(2)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&1u8)?;
-                seq.end()
-            }
-            PanicColumn(m) => {
-                let mut seq = serializer.serialize_tuple(3)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&2u8)?;
-                seq.serialize_element(&m.index)?;
-                seq.end()
-            }
-            StopColumn(m) => {
-                let mut seq = serializer.serialize_tuple(3)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&3u8)?;
-                seq.serialize_element(&m.index)?;
-                seq.end()
-            }
-            StartScene(m) => {
-                let mut seq = serializer.serialize_tuple(3)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&4u8)?;
-                seq.serialize_element(&m.index)?;
-                seq.end()
-            }
-            PanicSlot(m) => {
-                let mut seq = serializer.serialize_tuple(4)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&5u8)?;
-                seq.serialize_element(&m.column_index)?;
-                seq.serialize_element(&m.row_index)?;
-                seq.end()
-            }
-            StartSlot(m) => {
-                let mut seq = serializer.serialize_tuple(4)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&6u8)?;
-                seq.serialize_element(&m.column_index)?;
-                seq.serialize_element(&m.row_index)?;
-                seq.end()
-            }
-
-            StopSlot(m) => {
-                let mut seq = serializer.serialize_tuple(4)?;
-                seq.serialize_element(&self.pulse_diff)?;
-                seq.serialize_element(&7u8)?;
-                seq.serialize_element(&m.column_index)?;
-                seq.serialize_element(&m.row_index)?;
-                seq.end()
-            }
-        }
-    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
@@ -265,12 +171,12 @@ pub struct SignedMatrix {
 }
 
 impl SignedMatrix {
-    pub fn encode_value(matrix: &Matrix) -> Result<String, Box<dyn Error>> {
+    pub fn encode_value(matrix: &Matrix) -> Result<String, Box<dyn std::error::Error>> {
         let bytes = rmp_serde::to_vec_named(matrix)?;
         Ok(BASE64_ENGINE.encode(bytes))
     }
 
-    pub fn decode_value(&self) -> Result<Matrix, Box<dyn Error>> {
+    pub fn decode_value(&self) -> Result<Matrix, Box<dyn std::error::Error>> {
         let bytes = BASE64_ENGINE.decode(self.matrix.as_bytes())?;
         Ok(rmp_serde::from_slice(&bytes)?)
     }
@@ -1672,7 +1578,7 @@ type PlaytimeApiResult<T> = Result<T, PlaytimeApiError>;
 
 /// Important: Since some of these types are going to be used in real-time contexts, we don't
 /// want heap-allocated types in here!
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 #[error("{msg}")]
 pub struct PlaytimeApiError {
     msg: &'static str,
---
+++ playtime-api/src/persistence/serialization.rs
@@ -0,0 +1,171 @@
+use crate::persistence::{
+    MatrixSequenceColumnMessage, MatrixSequenceEvent, MatrixSequenceMessage,
+    MatrixSequenceRowMessage, MatrixSequenceSlotMessage,
+};
+use serde::de::{Error, SeqAccess, Visitor};
+use serde::ser::SerializeTuple;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::Formatter;
+
+/// A very compact representation of an event.
+///
+/// The compactness is achieved using tuple serialization.
+///
+/// Pro:
+///
+/// - If used with a CSV serializer, the outcome can be made look as "noise-less" as
+///   REAPER's MIDI sequence format (newline to separate events, space delimiter, unquoted strings).
+/// - If used with JSON/Lua serializer, the outcome is valid JSON/Lua while still being compact.
+///   No need for string embedding (which looks especially bad in JSON due to newline escaping).
+/// - If used with a binary serializer (e.g. bincode or msgpack), one can achieve a *really* compact
+///   serialization that  also tops REAPER's MIDI sequence format. That will come in handy with
+///   large undo histories or storage within RPP (in RPPs, we are base64-encoded, so the
+///   human-readable-text advantage is not present anyway).
+/// - TODO-high-ms3 Especially the last point could be desirable for MIDI sequences as well.
+///    Use serde for them, too!
+///
+/// Contra:
+///
+/// - Not self-describing. However: If embedded in some `Serialize` wrapper that evaluates
+///   `is_human_readable`, one could switch between this non-descriptive serialization style and a
+///   (derived) descriptive serialization style. So we can have both if we want.
+impl Serialize for MatrixSequenceEvent {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        use MatrixSequenceMessage::*;
+        match self.message {
+            PanicMatrix => {
+                let mut seq = serializer.serialize_tuple(2)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&0u8)?;
+                seq.end()
+            }
+            StopMatrix => {
+                let mut seq = serializer.serialize_tuple(2)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&1u8)?;
+                seq.end()
+            }
+            PanicColumn(m) => {
+                let mut seq = serializer.serialize_tuple(3)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&2u8)?;
+                seq.serialize_element(&m.index)?;
+                seq.end()
+            }
+            StopColumn(m) => {
+                let mut seq = serializer.serialize_tuple(3)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&3u8)?;
+                seq.serialize_element(&m.index)?;
+                seq.end()
+            }
+            StartScene(m) => {
+                let mut seq = serializer.serialize_tuple(3)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&4u8)?;
+                seq.serialize_element(&m.index)?;
+                seq.end()
+            }
+            PanicSlot(m) => {
+                let mut seq = serializer.serialize_tuple(4)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&5u8)?;
+                seq.serialize_element(&m.column_index)?;
+                seq.serialize_element(&m.row_index)?;
+                seq.end()
+            }
+            StartSlot(m) => {
+                let mut seq = serializer.serialize_tuple(4)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&6u8)?;
+                seq.serialize_element(&m.column_index)?;
+                seq.serialize_element(&m.row_index)?;
+                seq.end()
+            }
+
+            StopSlot(m) => {
+                let mut seq = serializer.serialize_tuple(4)?;
+                seq.serialize_element(&self.pulse_diff)?;
+                seq.serialize_element(&7u8)?;
+                seq.serialize_element(&m.column_index)?;
+                seq.serialize_element(&m.row_index)?;
+                seq.end()
+            }
+        }
+    }
+}
+
+struct MatrixSequenceEventVisitor;
+
+impl<'de> Visitor<'de> for MatrixSequenceEventVisitor {
+    type Value = MatrixSequenceEvent;
+
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        write!(formatter, "a tuple")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        use MatrixSequenceMessage::*;
+        macro_rules! col(() => {
+            MatrixSequenceColumnMessage {
+                index: seq
+                    .next_element()?
+                    .ok_or(Error::custom("expected column index"))?
+            }
+        });
+        macro_rules! row(() => {
+            MatrixSequenceRowMessage {
+                index: seq
+                    .next_element()?
+                    .ok_or(Error::custom("expected row index"))?
+            }
+        });
+        macro_rules! slot(() => {
+            MatrixSequenceSlotMessage {
+                column_index: seq
+                    .next_element()?
+                    .ok_or(Error::custom("expected slot column index"))?,
+                row_index: seq
+                    .next_element()?
+                    .ok_or(Error::custom("expected slot row index"))?,
+            }
+        });
+        let pulse_diff: u32 = seq
+            .next_element()?
+            .ok_or(Error::custom("expected pulse diff"))?;
+        let msg_type: u8 = seq
+            .next_element()?
+            .ok_or(Error::custom("expected message type"))?;
+        let message = match msg_type {
+            0 => PanicMatrix,
+            1 => StopMatrix,
+            2 => PanicColumn(col!()),
+            3 => StopColumn(col!()),
+            4 => StartScene(row!()),
+            5 => PanicSlot(slot!()),
+            6 => StartSlot(slot!()),
+            7 => StopSlot(slot!()),
+            _ => return Err(Error::custom(format!("unknown message type {msg_type}"))),
+        };
+        let event = MatrixSequenceEvent {
+            pulse_diff,
+            message,
+        };
+        Ok(event)
+    }
+}
+
+impl<'de> Deserialize<'de> for MatrixSequenceEvent {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_seq(MatrixSequenceEventVisitor)
+    }
+}
