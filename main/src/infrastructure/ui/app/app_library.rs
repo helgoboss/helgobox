a2a469ef2af7e5579729e16e160667401ce3c475
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-03-14 22:14:54 +0100

    Update versions

--- main/src/infrastructure/ui/app/app_library.rs
+++ main/src/infrastructure/ui/app/app_library.rs
@@ -733,9 +733,9 @@ fn to_status(err: anyhow::Error) -> Status {
     Status::unknown(err.to_string())
 }
 
-/// The minimum version of the app API that the host (ReaLearn) requires to properly
+/// The minimum version of the app API that the host (Helgobox) requires to properly
 /// communicates with it. Keep this up-to-date!
-pub const MIN_APP_API_VERSION: Version = Version::new(2, 0, 0);
+pub const MIN_APP_API_VERSION: Version = Version::new(3, 0, 0);
 
 #[cfg(not(feature = "playtime"))]
 fn playtime_not_available() -> Result<(), Status> {
