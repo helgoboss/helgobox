d5d2532077d71d59589d91598ffb98c918bebbd8
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-05-14 00:52:01 +0200

    Don't log repeated warnings as vertical dash
    this was a confusing solution for console spamming, we should replace it with
    a better one at some point

--- main/src/base/notification.rs
+++ main/src/base/notification.rs
@@ -1,8 +1,6 @@
-use once_cell::sync::Lazy;
 use reaper_high::Reaper;
 use reaper_medium::{MessageBoxType, ReaperStringArg};
 use std::error::Error;
-use std::sync::Mutex;
 
 pub fn notify_processing_result(heading: &str, msgs: Vec<String>) {
     let joined_msg = msgs.join("\n\n");
@@ -26,15 +24,7 @@ pub fn warn_user_about_anyhow_error(error: anyhow::Error) {
 }
 
 pub fn warn(msg: String) {
-    static PREV_MSG: Lazy<Mutex<String>> = Lazy::new(Default::default);
-    let mut prev_msg = PREV_MSG.lock().unwrap();
-    let reaper = Reaper::get();
-    if msg == *prev_msg {
-        reaper.show_console_msg("|");
-    } else {
-        reaper.show_console_msg(format!("\n\nReaLearn warning: {msg} "));
-        *prev_msg = msg;
-    }
+    Reaper::get().show_console_msg(format!("\n\nReaLearn warning: {msg} "));
 }
 
 #[allow(dead_code)]
