a68c72531f804b3198f8e9251fe55e9d7c45929a
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-12-12 09:04:56 +0100

    #1348 Also generate one text icon

--- artwork-processor/src/main.rs
+++ artwork-processor/src/main.rs
@@ -15,49 +15,48 @@ fn main() -> Result<()> {
 
 fn render_artwork() -> Result<()> {
     let logo_file = "resources/artwork/playtime-logo.svg";
-    generate_icons("playtime", logo_file, "")?;
-    generate_icons("playtime-custom", logo_file, "with-settings-icon")?;
+    let logo_svg = fs::read_to_string(logo_file)?;
+    generate_icons("playtime", &logo_svg, "")?;
+    generate_icons("playtime-custom", &logo_svg, "with-settings-icon")?;
+    let logo_svg_with_text_2 = logo_svg.replace("TEXT_PLACEHOLDER", "2");
+    generate_icons("playtime-2", &logo_svg_with_text_2, "with-text-badge")?;
     Ok(())
 }
 
-fn generate_icons(
-    name_with_dashes: &str,
-    src_file: &str,
-    additional_root_classes: &str,
-) -> Result<()> {
+fn generate_icons(name_with_dashes: &str, svg: &str, additional_root_classes: &str) -> Result<()> {
     let name_with_underscores = name_with_dashes.replace('-', "_");
     // Toolbar icons
-    generate_toolbar_icons(&name_with_underscores, src_file, additional_root_classes)?;
+    generate_toolbar_icons(&name_with_underscores, svg, additional_root_classes)?;
     // Icons for docs
     generate_icon(
-        src_file,
+        svg,
         format!("doc/playtime/modules/ROOT/images/screenshots/{name_with_dashes}-toolbar-icon.png"),
         (120, 120),
-        "",
+        additional_root_classes,
         &[ToolbarIconStatus::Normal],
     )?;
     Ok(())
 }
 
-fn generate_toolbar_icons(name: &str, src_file: &str, additional_root_classes: &str) -> Result<()> {
+fn generate_toolbar_icons(name: &str, svg: &str, additional_root_classes: &str) -> Result<()> {
     use ToolbarIconStatus::*;
     let toolbar_statuses = [Normal, Hovered, Selected];
     generate_icon(
-        src_file,
+        svg,
         format!("resources/artwork/toolbar_icons/toolbar_{name}.png"),
         (30, 30),
         additional_root_classes,
         &toolbar_statuses,
     )?;
     generate_icon(
-        src_file,
+        svg,
         format!("resources/artwork/toolbar_icons/150/toolbar_{name}.png"),
         (45, 45),
         additional_root_classes,
         &toolbar_statuses,
     )?;
     generate_icon(
-        src_file,
+        svg,
         format!("resources/artwork/toolbar_icons/200/toolbar_{name}.png"),
         (60, 60),
         additional_root_classes,
@@ -67,15 +66,14 @@ fn generate_toolbar_icons(name: &str, src_file: &str, additional_root_classes: &
 }
 
 fn generate_icon(
-    src_file: impl AsRef<Path>,
+    svg: &str,
     dst_file: impl AsRef<Path>,
     (width, height): (u32, u32),
     additional_root_classes: &str,
     statuses: &[ToolbarIconStatus],
 ) -> Result<()> {
     let dst_file = dst_file.as_ref();
-    let svg = fs::read_to_string(src_file)?;
-    let pixmap = render_toolbar_icon(&svg, (width, height), additional_root_classes, statuses)?;
+    let pixmap = render_toolbar_icon(svg, (width, height), additional_root_classes, statuses)?;
     fs::create_dir_all(dst_file.parent().context("no parent file")?)?;
     pixmap.save_png(dst_file)?;
     Ok(())
