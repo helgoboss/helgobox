af03a310c15e3447c77434360c6eb868e8eb13c8
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-12-13 16:21:58 +0100

    Raise versions

--- main/src/infrastructure/proto/mod.rs
+++ main/src/infrastructure/proto/mod.rs
@@ -41,4 +41,4 @@ pub use initial_playtime_events::*;
 /// It's important to get this right in order to get good error messages from customers. Knowing
 /// that they just have an incompatible version mix (e.g. due to manual installing) makes it
 /// trivial to respond to bug reports.
-pub const HOST_API_VERSION: &str = "12.0.0";
+pub const HOST_API_VERSION: &str = "13.0.0";
