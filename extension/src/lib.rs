814c54499f934e258b6c323423beee41c39afad1
Benjamin Klum <benjamin.klum@helgoboss.org>
2025-01-03 14:29:58 +0000

    #1391 Make Helgobox helper extension work on Linux as well

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -22,6 +22,14 @@ type ReaperPluginEntry = unsafe extern "C" fn(
     rec: *mut ::reaper_low::raw::reaper_plugin_info_t,
 ) -> ::std::os::raw::c_int;
 
+type SwellDllMain = unsafe extern "C" fn(
+    hinstance: reaper_low::raw::HINSTANCE,
+    reason: u32,
+    get_func: Option<
+        unsafe extern "C" fn(name: *const std::os::raw::c_char) -> *mut std::os::raw::c_void,
+    >,
+) -> ::std::os::raw::c_int;
+
 struct HelgoboxExtension {
     /// Just for RAII.
     _plugin_library: Option<Library>,
@@ -76,6 +84,19 @@ fn eagerly_load_plugin_lib(context: &PluginContext) -> Result<Library> {
         .context("couldn't find plug-in library")?;
     // Load shared library
     let plugin_library = unsafe { Library::new(plugin_library_path)? };
+    #[cfg(target_os = "linux")]
+    {
+        // Linux only: Run SWELL entry point of library (on Windows, SWELL is not necessary, and on
+        // macOS, SWELL is obtained in a different way)
+        let swell_dll_main: Symbol<SwellDllMain> = unsafe { plugin_library.get(b"SWELL_dllMain")? };
+        unsafe {
+            swell_dll_main(
+                context.h_instance(),
+                reaper_low::raw::DLL_PROCESS_ATTACH,
+                context.swell_function_provider(),
+            );
+        }
+    }
     // Run extension entry point of library
     let reaper_plugin_entry: Symbol<ReaperPluginEntry> =
         unsafe { plugin_library.get(b"ReaperPluginEntry")? };
