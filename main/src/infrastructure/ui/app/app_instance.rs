9a2045f8a0f9d7a486dd5416be4eab089ce9e3d9
Benjamin Klum <benjamin.klum@helgoboss.org>
2026-09-15 19:02:37 +0200

    Linux: For now use separate-process app instance

--- main/src/infrastructure/ui/app/app_instance.rs
+++ main/src/infrastructure/ui/app/app_instance.rs
@@ -94,8 +94,8 @@ pub fn create_shared_app_instance(instance_id: InstanceId) -> SharedAppInstance 
         // Therefore: Standalone mode on macOS!
         share(InProcessStandaloneAppInstance::new(instance_id))
     } else {
-        share(InProcessStandaloneAppInstance::new(instance_id))
-        // share(SeparateProcessAppInstance::new(instance_id))
+        // share(InProcessStandaloneAppInstance::new(instance_id))
+        share(SeparateProcessAppInstance::new(instance_id))
     }
 }
 
